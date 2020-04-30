bc2e1512d422b9dcd9f23ef74ac7df83309b9a8b
Tom Chantler <TomChantler@users.noreply.github.com>
2020-04-30 23:45:57 +0100

    First working version

---
+++ GhostDataFix.ps1
@@ -0,0 +1,34 @@
+﻿# By Tom Chantler - https://tomssl.com/how-to-upgrade-your-ghost-blog-to-the-latest-version-without-breaking-anything-part-1
+param([string]$DataFilePath
+    , [bool]$FixMarkdownLinks = $true
+    , [bool]$ConvertImageLinksToLowerCase = $false
+    , [string]$ImagesDirectory = $null)
+
+Write-Host "About to apply fixes to $DataFilePath" -ForegroundColor Yellow
+
+$fixedDataFilePath = $DataFilePath.Insert($DataFilePath.LastIndexOf("."), $(if ($ConvertImageLinksToLowerCase) { ".imagesfixed" }) + $(if ($FixMarkdownLinks) { ".markdownfixed" }))
+if ($fixedDataFilePath -eq $DataFilePath) {
+    return
+}
+$patternForMarkdownLinks = "(?<linkpart>\[[^\]}]*\]*)\s*\((?<scheme>https?:\/\/)?(?<address>[^\[\""]*?)(?:[\\])?(?<target>""\s*target\s*=\s*\\*\""\s*_.*?)\)"
+$patternForImageLinks = [Regex]"(/content\S+?\.(jpg|png|gif|svg))"
+[IO.Directory]::SetCurrentDirectory((Convert-Path (Get-Location -PSProvider FileSystem)))
+$data = (Get-Content $DataFilePath -Encoding UTF8) -Join [Environment]::NewLine
+if ($ConvertImageLinksToLowerCase) { 
+    $data = $patternForImageLinks.Replace($data, { $args[0].Value.ToLower() })
+    Write-Host 'Fixed Image Links' -ForegroundColor Yellow
+}
+if ($FixMarkdownLinks) { 
+    $data = $data -replace $patternForMarkdownLinks, '${linkpart}(${scheme}${address})'
+    Write-Host 'Fixed Markdown "target ="_blank trick' -ForegroundColor Yellow
+}
+if (Test-Path $ImagesDirectory -PathType Container) {
+    Get-ChildItem $ImagesDirectory -recurse | Where-Object { -Not $_.PSIsContainer } | Rename-Item -NewName { $_.FullName.ToLower() }
+    Write-Host "Renamed all files in $ImagesDirectory directory to lower case" -ForegroundColor Yellow
+} 
+if ($PSVersionTable.PSVersion.Major -ge 6){
+Set-Content -Path $fixedDataFilePath -Value $data -Encoding UTF8NoBOM
+} else {
+    [IO.File]::WriteAllText($fixedDataFilePath, $data)
+}
+Write-Host Done -ForegroundColor Green
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Ghost-Data-Fix
+
+Simple PowerShell Script to fix the data exported from a Ghost blog so that it can be reimported.
+
+Full instructions at https://tomssl.com/how-to-upgrade-your-ghost-blog-to-the-latest-version-without-breaking-anything-part-1
